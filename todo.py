80388c6cb093e8434abf9e2349e0160dab7bf6a3
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2017-03-14 03:14:18 -0600

    Task #8 - Decorating for new api.

--- todo.py
+++ todo.py
@@ -68,21 +68,10 @@ def login():
 
 @route('/new', method='GET')
 def new_item():
-
     if request.GET.get('save','').strip():
-
-        new = request.GET.get('task', '').strip()
-        conn = sqlite3.connect('todo.db')
-        c = conn.cursor()
-
-        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
-        new_id = c.lastrowid
-
-        conn.commit()
-        c.close()
-
-        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
-
+        createNewTask = validateDecorator(insertNewTask)
+        task = request.GET.get('task', '').strip()
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % createNewTask({'task': task})
     else:
         return template('new_task.tpl')
 
